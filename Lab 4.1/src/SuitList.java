939a5d6ca6a0b1cd66cb1894654befefc9da10a3
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-12-14 13:10:30 -0500

    Suit list

---
+++ Lab 4.1/src/SuitList.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class SuitList {
+   public static void main(String args[]) {
+
+	  ArrayList<String> obj = new ArrayList<String>();
+
+	  /*This is how elements should be added to the array list*/
+
+	  obj.add("Hearts");
+	  obj.add("Diamonds");
+	  obj.add("Clubs");
+	  obj.add("Spades");
+
+	  System.out.println("Suit list is:"+obj);
+   }
+}
