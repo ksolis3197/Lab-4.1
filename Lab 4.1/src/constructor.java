08fef051d35e3fdbc7f0b21184f9b1449d868a9b
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-12-15 13:07:48 -0500

    constructor

---
+++ Lab 4.1/src/constructor.java
@@ -0,0 +1,24 @@
+
+public class constructor {
+
+	   int pv;  
+	   String suite;
+	   String rank;
+		    
+	   //parameterized constructor with three parameters
+	   constructor(int pointValue, String suite, String rank){  
+	       this.pv = pointValue;  
+	       this.suite = suite;  
+	       this.rank = rank;
+	   }  
+	   void info(){
+	        System.out.println("rank: "+rank+" suit: "+suite+" pointValue: "+pv);
+	   }  
+		   
+	   public static void main(String args[]){  
+		constructor obj1 = new constructor(2,"Ace","Clubs");  
+		constructor obj2 = new constructor(10,"King","Clubs");  
+		obj1.info();  
+		obj2.info();  
+	   }  
+	}
